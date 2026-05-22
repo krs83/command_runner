2b42253af2f04630ee2b4a0f5933d984cf595f32
krs83 <krs83@yandex.ru>
2026-05-22 11:42:29 +0300

    first init

---
+++ lib/command_runner.dart
@@ -0,0 +1,9 @@
+/// A simple command runner to handle command-line arguments.
+///
+/// More extensive documentation for this library goes here.
+library;
+
+export 'src/command_runner_base.dart';
+export 'src/arguments.dart';
+export 'src/help_command.dart';
+// TODO: Export any other libraries intended for clients of this package.
